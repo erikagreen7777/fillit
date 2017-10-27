ce7dbb62fd890dd782aa8afa23a96ecdf1958a48
Erika Green <egreen@e1z2r9p42.42.us.org>
2017-10-26 17:10:20 -0700

    tet_length normed

--- Desktop/ourfillit/srcs/tet_length.c
+++ Desktop/ourfillit/srcs/tet_length.c
@@ -6,7 +6,7 @@
 /*   By: egreen <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/25 09:09:34 by egreen            #+#    #+#             */
-/*   Updated: 2017/10/25 09:10:15 by egreen           ###   ########.fr       */
+/*   Updated: 2017/10/26 17:09:51 by egreen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,18 @@ int		tet_length(char *str)
 	len = ft_strlen(str);
 	i = 20;
 	if (len > 546 || len < 19)
-		return(printf("Error: invalid file (length)\n")); // return (1);
-	else //
-		printf("The length is fine\n"); //
-	ft_putendl(str); //	
+		return (2);
 	if ((str[len - 1]) == '\n' && (str[len - 2] == '\n'))
-		return(printf("Error: last char isn't a newline\n")); // return (1);
+		return (2);
 	while (i < len)
 	{
 		if (str[i] != '\n')
-			return(printf("Error: No newline at str[20]")); //return (1);
+			return (2);
 		if (str[i + 1] == '\n')
-			return(printf("Error: str[21] shouldn't be a newline")); // return (1);
+			return (2);
 		i += 21;
 	}
 	if ((str[0] != '.') && (str[0] != '#'))
-		return(printf("The first char isn't valid")); //return (1);
+		return (2);
 	return (0);
 }
