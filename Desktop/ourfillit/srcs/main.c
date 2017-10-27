1c32f85fe0f0601f685d1225cdc46cf1c6ce62e1
Erika Green <egreen@e1z2r9p42.42.us.org>
2017-10-26 17:12:28 -0700

    main normed

--- Desktop/ourfillit/srcs/main.c
+++ Desktop/ourfillit/srcs/main.c
@@ -1,11 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egreen <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/26 17:11:34 by egreen            #+#    #+#             */
+/*   Updated: 2017/10/26 17:12:13 by egreen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fillit.h"
 
-int	main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-	int		fd;
-	int		i;
-	char	buf[601];
+	int			fd;
+	int			i;
+	char		buf[601];
+	t_tet_frag	*tetromino;
 
+	tetromino = ft_memalloc(sizeof(t_tet_frag));
 	i = 0;
 	if (argc == 2)
 	{
@@ -13,17 +27,11 @@ int	main(int argc, char **argv)
 			write(1, "error\n", 6);
 		ft_bzero(buf, 601);
 		read(fd, buf, 600);
-		validswitch(buf);
+		validswitch(buf, tetromino);
+		ft_putchar('\n');
 		close(fd);
 	}
 	else
 		write(1, "usage: ./fillit [test file]\n", 28);
 	return (0);
 }
-
-
-
-
-
-
-
