2815a0af153c84622f3b1d7f3030fa7a7c286d10
Erika Green <egreen@e1z2r9p42.42.us.org>
2017-10-26 17:07:54 -0700

    normed validswitch

--- Desktop/ourfillit/srcs/validswitch.c
+++ Desktop/ourfillit/srcs/validswitch.c
@@ -6,49 +6,37 @@
 /*   By: egreen <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/25 08:23:20 by egreen            #+#    #+#             */
-/*   Updated: 2017/10/25 08:38:30 by egreen           ###   ########.fr       */
+/*   Updated: 2017/10/26 17:07:20 by egreen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int		validswitch(char *buf)
+int		validswitch(char *buf, t_tet_frag *tetromino)
 {
-	size_t		i;
-	char		*str;
-	t_tet_frag	*tetromino;
+	size_t	i;
+	char	*str;
 	char	**new_g;
+	int		tetcount;
 
-	tetromino = ft_memalloc(sizeof(t_tet_frag));
+	tetcount = 0;
 	i = 20;
 	str = buf;
-	if ((tet_length(str) == 1))
+	if ((tet_length(str) == 2))
 	{
-		ft_putstr("invalid");
+		ft_putstr("error");
 		return (0);
 	}
 	str = ft_remove_newline(str);
-	ft_putendl(str);
 	if (tet_all(str, tetromino) == 2)
 	{
-		ft_putstr("invalid");
+		ft_putstr("error");
 		return (0);
 	}
-	// printf("tetromino->count = %d\n", tetromino->count);
-	
-	// printf("%d\n", tetcount); //	
-	// int tetcount = tetromino->count;
-	int tetcount = 0;
-
-	ft_putstr(tetromino->array[0]);
 	tetromino->size = MINGRID;
 	new_g = new_grid(tetromino->size);
 	while (!solver(new_g, tetromino, tetcount, tetromino->size))
-	{
 		new_g = new_grid(++tetromino->size);
-	}
 	print_grid(new_g, tetromino->size);
-	 //print result
-	// new_g[2][2] = 'A';
 	return (0);
 }
