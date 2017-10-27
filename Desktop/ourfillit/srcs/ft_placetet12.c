5b94f4e3b83c6a6b571d414fc3966bc093dc325c
Erika Green <egreen@e1z2r9p42.42.us.org>
2017-10-26 17:39:57 -0700

    added ft_placetet12

--- Desktop/ourfillit/srcs/ft_placetet12.c
+++ Desktop/ourfillit/srcs/ft_placetet12.c
@@ -6,43 +6,50 @@
 /*   By: nwang <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/23 16:01:43 by nwang             #+#    #+#             */
-/*   Updated: 2017/10/24 21:43:03 by nwang            ###   ########.fr       */
+/*   Updated: 2017/10/26 17:39:13 by egreen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include "fillit.h"
 
-int		ft_checktet12(char **g, int x, int y)
+int		ft_checktet12(char **g, int *yx)
 {
-	int size;
+	int		size;
+	int		y;
+	int		x;
 
-	size = ft_strlen(g[0]);
+	y = yx[0];
+	x = yx[1];
+	size = yx[2];
 	if (x == 0)
 		return (0);
-	if(ft_strlen(&g[0][x]) < 2)
+	if (ft_strlen(&g[0][x]) < 2)
 		return (0);
 	if (!(size - y >= 2))
 		return (0);
-	if(!(g[y][x] == '.'))
+	if (!(g[y][x] == '.'))
 		return (0);
 	y++;
-	if(!(g[y][x] == '.'))
+	if (!(g[y][x] == '.'))
 		return (0);
 	x--;
-	if(!(g[y][x] == '.'))
+	if (!(g[y][x] == '.'))
 		return (0);
 	x++;
-	x++;
-	if(!(g[y][x] == '.'))
+	if (!(g[y][++x] == '.'))
 		return (0);
 	return (1);
 }
 
-int		ft_placetet12(char **g, int x, int y, char c)
+int		ft_placetet12(char **g, int *yx, char c)
 {
-	if (ft_checktet12(g, x, y) == 0)
-		return (0);	
+	int y;
+	int x;
+
+	y = yx[0];
+	x = yx[1];
+	if (ft_checktet12(g, yx) == 0)
+		return (0);
 	g[y][x] = c;
 	y++;
 	g[y][x] = c;
